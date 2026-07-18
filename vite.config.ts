6b9b0cdcaff585a4bd9785589e4b99a870ca541d
NontobekoNcube <nontoncube7@gmail.com>
2026-07-18 11:30:40 +0200

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
+//https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    base: '/future_fit_calculator/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
